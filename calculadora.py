d12760a556063dd497a8b7dfeac4426789f54a57
walisson69 <walissonsantost008@gmail.com>
2024-09-12 22:38:32 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,48 +1,44 @@
-print('=-=' * 20)
-print('                   CALCULADORA   ')
-print('=-=' * 20)
-print('''ESCOLHA UMA DESSAS OPÇÕES
-      [0]Para fechar o programa
-      [1]Adição
-      [2]Subtração
-      [3]Multiplicação
-      [4]Divisão
-      [5]Potenciação''')
-     
+import os
+import time
+
 while True:
-    num = int(input('Opção: '))
-    if num == 1:
-        print('Você escolheu Adição!')
-        n1 = int(input('Digite um numero: '))
-        n2 = int(input('Digite outro: '))
-        nr = n1 + n2
-        print(f'{n1} + {n2} = {nr}')
-    elif num == 2:
-        print('Você escolheu Subtração!')
-        sub = int(input('Digite um numero: '))
-        sub2 = int(input('Digite outro: '))
-        subr = sub -  sub2
-        print(f'{sub} - {sub2} = {subr}')
-    elif num == 3:
-        print('Você escolheu Multiplicação!')
-        mult = int(input('Digite um numero: '))
-        mult2 = int(input('Digite outro: '))
-        multr = mult * mult2
-        print(f'{mult} X {mult2} = {multr}')
-    elif num == 4:
-        print('Você escolheu Divisão!')
-        div = float(input('Digite um numero: '))
-        div2 = float(input('Digite outro: '))
-        divr = div / div2
-        print(f'{div} / {div2} = {divr:.1f}')
-    elif num == 5:
-        print('Você escolheu Potenciação!')
-        pot = int(input('Digite a base: '))
-        pote = int(input('Digite o expoente: '))
-        pottotal = pot ** pote
-        print(f'O resulatado da potencia {pot} e {pote} é {pottotal}')
-    elif num == 0:
-        print('Sessâo finalizada. Volte sempre!')
+    try:
+        numero1 = int(input('Digite um numero: '))
+        numero2 = int(input('Digite outro numero: '))
+        print('''Opções aritméticas:
+[+]Soma
+[-]Subtração
+[x]Multiplicação
+[/]Divisão
+[F]Para fechar''')
+        escolha_user = input('Escolha uma das opções listadas acima: ').upper()
+    except:
+        os.system('cls')
+        print('Por favos digite apenas numeros inteiros')
+        continue
+    
+    if escolha_user == '+':
+        os.system('cls')
+        soma_total = numero1 + numero2
+        print(f'A soma entre {numero1} e {numero2} é {soma_total}')
+
+    elif escolha_user == '-':
+        os.system('cls')
+        soma_total = numero1 - numero2
+        print(f'A subtração entre {numero1} e {numero2} é {soma_total}')
+
+    elif escolha_user == 'X':
+        os.system('cls')
+        soma_total = numero1 * numero2
+        print(f'A multiplicação entre {numero1} e {numero2} é {soma_total}')
+    
+    elif escolha_user == '/':
+        os.system('cls')
+        soma_total = numero1 / numero2
+        print(f'A divisão entre {numero1} e {numero2} é {soma_total:.2f}')
+    elif escolha_user == 'F':
+        os.system('cls')
+        print('ENCERRANDO CALCULADORA!!....')
+        time.sleep(3)
+        print('PROGRAMA ENCERRADO!!')
         break
-    else:
-        print('Tente novamente. Essa opção não existe!!')
